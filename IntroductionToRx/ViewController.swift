d38bd6a2b9d6f2b1412ab884f7f9a633526c3179
Diego Perini <diego@dperini.com>
2018-11-05 19:03:53 +0300

    Added flatMap examples

--- IntroductionToRx/ViewController.swift
+++ IntroductionToRx/ViewController.swift
@@ -12,17 +12,27 @@ import RxCocoa
 import Alamofire
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        arrayExample()
-        observableExample()
+    let loginButton = UIButton()
+    let usernameText = UITextView()
+    let passText = UITextView()
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //        arrayExample()
+        //        observableExample()
+        //
+        //        coldObservableCreate()
+        //        hotObservableCreate()
         
-        coldObservableCreate()
-        hotObservableCreate()
+        //        incorrectHttpCallChaining()
+        correctHttpCallChaining()
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        disposeBag = DisposeBag()
+    }
+    
     func arrayExample() {
         let myArray = [1, 2, 3]
         let transformation = myArray.map { e -> Int in
@@ -33,13 +43,13 @@ class ViewController: UIViewController {
             print("Filter: ", e)
             return e > 2
         }
-
+        
         filtered.forEach { e in
             print("Result: ", e)
         }
         print("Done!")
     }
-
+    
     func observableExample() {
         let myObservable = Observable.of(1, 2, 3) // or Observable.from([1, 2, 3])
         let transformation = myObservable.map { e -> Int in
@@ -50,20 +60,20 @@ class ViewController: UIViewController {
             print("Filter: ", e)
             return e > 2
         }
-
+        
         _ = filtered.subscribe(
             onNext: { e in
                 print("Result: ", e)
-            },
+        },
             onError: { err in
                 print(err)
-            },
+        },
             onCompleted: {
                 print("Done!")
-            },
+        },
             onDisposed: {
                 print("Garbage collected!")
-            }
+        }
         )
     }
     
@@ -102,5 +112,108 @@ class ViewController: UIViewController {
             onCompleted: { print("this is never called") },
             onDisposed: nil
         )
+    }
+    
+    func incorrectHttpCallChaining() {
+        httpPostRx(url: "https://httpbin.org/get?call=login", body: [ "user": "example", "pass": "123456" ])
+            .subscribe(
+                onNext: { _ in
+                    _ = httpGetRx(url: "https://httpbin.org/get?call=getProfile").subscribe(
+                        onNext: { profile in
+                           // show profile
+                        },
+                        onError: { e in
+                            // show error
+                        },
+                        onCompleted: nil,
+                        onDisposed: nil
+                    )
+                },
+                onError: { e in
+                    // show error
+                },
+                onCompleted: nil,
+                onDisposed: nil
+            )
+            .disposed(by: disposeBag)
+        
+    }
+    
+    func correctHttpCallChaining() {
+        loginButton.rx.controlEvent(.touchUpInside)
+            .flatMap { _ in
+                return httpPostRx(url: "https://httpbin.org/get?call=login", body: [ "user": self.usernameText.text, "pass": self.passText.text ])
+            }
+            .flatMap { l in
+                return httpGetRx(url: "https://httpbin.org/get?call=getProfile")
+                    .map { p in
+                        return (login: l, profile: p)
+                    }
+            }
+            .flatMap { lp in
+                return httpGetRx(url: "https://httpbin.org/get?call=getNotifications")
+                    .map { n in
+                        return (login: lp.login, profile: lp.profile, notifications: n)
+                    }
+            }
+            .flatMap { lpn in
+                return httpGetRx(url: "https://httpbin.org/get?call=getSettings")
+                    .map { s in
+                        return (login: lpn.login, profile: lpn.profile, notifications: lpn.notifications, settings: s)
+                    }
+            }
+            .subscribe(
+                onNext: { lpns in
+                    print(lpns.login)
+                    print(lpns.profile)
+                    print(lpns.notifications)
+                    print(lpns.settings)
+                    // show everything
+                },
+                onError: { e in
+                    // show error
+                },
+                onCompleted: nil,
+                onDisposed: nil
+            )
+            .disposed(by: disposeBag)
+    }
+}
+
+func httpGetRx(url: String) -> Observable<Any> {
+    return Observable<Any>.create { sub in
+        Alamofire.request(url)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    sub.onNext(response.result.value!)
+                    sub.onCompleted()
+                case .failure(let error):
+                    sub.onError(error)
+                }
+        }
+        
+        return Disposables.create()
+    }
+}
+
+func httpPostRx(url: String, body: [String: Any]) -> Observable<Any> {
+    return Observable<Any>.create { sub in
+        Alamofire.request(url, method: .post, parameters: body)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    sub.onNext(response.result.value!)
+                    sub.onCompleted()
+                case .failure(let error):
+                    sub.onError(error)
+                }
+        }
+        
+        return Disposables.create()
     }
 }
